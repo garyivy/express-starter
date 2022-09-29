15598aae00daa90c8487f23c1509868c6a60a804
garyivy <gary.ivy@wwt.com>
2022-09-29 12:06:14 -0500

    slight refactor and updated README - gi

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # express-starter
-Starter Kit for Express Api with Typescript, Unit and Integration Testing
+
+Starter Kit for Express Api with Typescript, Unit and Integration Testing, Automatic Swagger generation and Open API validation
+
+## To use as developer
+
+`npm install`</br>
+`npm run tsoa:gen`</br>
+`npm run dev`</br>
+
+## How does tsoa work?
+
+It scans your controllers for annotations, interfaces, types, and comments and generates a swagger file and a file to registers routes (links app route to controller methods)
+
+## How does open api validator work?
+
+It compares incoming request against the swagger document and returns 400 (BAD REQUEST) for input that does not match the swagger spec
--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,35 @@
 import express from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { RegisterRoutes } from './generated/routes';
 import swaggerUi from 'swagger-ui-express';
-const swaggerFile = require('./generated/swagger.json');
+import swaggerFile from './generated/swagger.json';
 import * as OpenApiValidator from 'express-openapi-validator';
 import { installMiddleware } from './middleware/installMiddleware';
+import { logger } from './services/loggingService';
 
 const app = express();
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const defaultErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+  logger.error(`Unable to process request ${req.method} ${req.route}, ${err}`);
+  res.status(err.status || 500).json({
+    message: 'Unable to process the request at this time',
+  });
+};
+
+app.use(defaultErrorHandler);
 app.use(express.json());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
+// TODO: Wby doesOpenApiValidator apiSpec need src/dist as part of path?
+const apiSpec = process.env.NODE === 'production' ? './dist/generated/swagger.json' : './src/generated/swagger.json';
 app.use(
   OpenApiValidator.middleware({
-    apiSpec: './src/generated/swagger.json', // TODO: Why do I have to add src to the path and how will this work on the dist path?
+    apiSpec,
     validateRequests: true,
     validateResponses: false,
   }),
 );
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((err, req, res, next) => {
-  // Global Error Handler
-  res.status(err.status || 500).json({
-    message: err.message,
-    errors: err.errors,
-  });
-});
 
 installMiddleware(app);
 RegisterRoutes(app);
